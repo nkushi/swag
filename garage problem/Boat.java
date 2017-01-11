45971ffa2f207f5a74fdb1b9506cb4ee19f44d64
nabeel <nkushi94@hotmail.com>
2017-01-11 16:45:55 +0000

    boat

---
+++ garage problem/Boat.java
@@ -0,0 +1,20 @@
+package Garage;
+
+public class Boat extends Vehicle {
+	
+	boolean hasPropellers;
+
+	
+	Boat(boolean hasPropellers, String make, String color, int id)
+	{
+		this.hasPropellers = hasPropellers;
+		this.make = make;
+		this.color = color;
+		this.id = id;
+	}
+	
+	private void takeInput()
+	{
+		int input = InputClass.nextInt();
+	}
+}
